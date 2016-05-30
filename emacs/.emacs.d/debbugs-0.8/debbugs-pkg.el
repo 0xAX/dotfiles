67f64eb646a20e9258013c1278ced7f5001690dd
Alexander Kuleshov <kuleshovmail@gmail.com>
2016-05-30 14:57:54 +0600

    add toml mode

--- emacs/.emacs.d/debbugs-0.8/debbugs-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from debbugs.el
-(define-package "debbugs" "0.8" "SOAP library to access debbugs servers" 'nil :url "http://elpa.gnu.org/packages/debbugs.html" :keywords '("comm" "hypermedia"))
--- emacs/.emacs.d/latex-preview-pane/latex-preview-pane-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "latex-preview-pane" "20151021" "Makes LaTeX editing less painful by providing a updatable preview pane" (quote nil))
-
-
-
