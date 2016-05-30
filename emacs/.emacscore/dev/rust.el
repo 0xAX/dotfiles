f974804b59f63970e8ed2886e6d029a489d25a8a
Alexander Kuleshov <kuleshovmail@gmail.com>
2016-05-30 16:48:53 +0600

    update rust mode

--- emacs/.emacscore/dev/rust.el
+++ emacs/.emacscore/dev/rust.el
@@ -1,3 +1,16 @@
-;;(add-to-list 'load-path "~/.emacs.d/lisp/")
-;;(autoload 'rust-mode "rust-mode" nil t)
-;;(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+(add-hook 'rust-mode-hook
+  (lambda ()
+    (setq indent-tabs-mode nil)
+    (setq tab-width 4)
+    (setq rust-indent-offset 4)))
+
+(setq racer-cmd "~/.cargo/bin/racer")
+(setq racer-rust-src-path "~/dev/rust/src")
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'rust-mode-hook #'company-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+
+(setq company-tooltip-align-annotations t)
