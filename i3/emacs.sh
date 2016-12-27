7231fdb472557487363fd8235902a7f48d48aacf
Alexander Kuleshov <kuleshovmail@gmail.com>
2016-12-28 00:57:27 +0600

    update i3 configuration

--- i3/emacs.sh
+++ i3/emacs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+EMACS_RUNNED=$(ps ax | grep emacs | awk '{ print $5 }' | sed -n '1p')
+
+if [ "$EMACS_RUNNED" != "emacs" ]
+then
+    emacs &
+    sleep 1;
+fi
+
 i3-msg workspace number 2;
 i3-msg mode passthrough;
-
