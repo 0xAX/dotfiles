07b656e53a0789ee2af388c2771a9ab4b52d1b3c
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-01-14 01:16:20 +0600

    emacs: remove monky

--- emacs/.emacs.d/monky/monky-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; monky-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("monky.el") (21855 28670 474691 228000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; monky-autoloads.el ends here
