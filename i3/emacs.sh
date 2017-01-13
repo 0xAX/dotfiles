78bf52bcc2147278a7f58d09e5ef2970d274a783
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-01-13 23:51:40 +0600

    i3: check emacs process by basename
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- i3/emacs.sh
+++ i3/emacs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EMACS_RUNNED=$(ps ax | grep emacs | awk '{ print $5 }' | sed -n '1p')
+EMACS_RUNNED=$(ps ax | grep emacs | awk '{ print $5 }' | sed -n '1p' | xargs basename)
 
 if [ "$EMACS_RUNNED" != "emacs" ]
 then
@@ -8,5 +8,5 @@ then
     sleep 1;
 fi
 
-i3-msg workspace number 2;
-i3-msg mode passthrough;
+i3-msg mode passthrough
+i3-msg workspace number 2
