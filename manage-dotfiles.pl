9c55c90c922eb4c6f1adb73dd6532fcae9b96968
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-06-17 13:52:56 +0600

    update usage of manage-dotfiles.pl

--- manage-dotfiles.pl
+++ manage-dotfiles.pl
@@ -4,18 +4,36 @@
 #
 # USAGE:
 #
-#       ./manage-dotfiles.pl -i - install dotfiles from this directory
-#                                 to the local system.
-#       ./manage-dotfiles.pl -u - update the repo from this directory.
+#       manage-dotfiles.pl --install - install dotfiles from this directory
+#                                      or directory pointed by the 
+#                                      $DOTFILES_ROOT_DIR to the local system.
+#       manage-dotfiles.pl --update  - put dotfiles from the system to the 
+#                                      repo.
+#
+# Enviroment variables used by manage-dotfiles.pl:
+#
+#   * DOTFILES_ROOT_DIR - path to this repo.
+#
 
 use strict;
 
 sub usage {
     print STDOUT <<USAGE_END;
 USAGE:
-  manage-dotfiles.pl -i - install dotfiles from this directory
-                            to the local system.
-  manage-dotfiles.pl -u - update the repo from this directory.
+  manage-dotfiles.pl --install - install dotfiles from this directory
+                                 or directory pointed by the 
+                                 \$DOTFILES_ROOT_DIR to the local system.
+  manage-dotfiles.pl --update  - put dotfiles from the system to the 
+                                 repo.
+
+Enviroment variables used by manage-dotfiles.pl:
+
+   * DOTFILES_ROOT_DIR - path to this repo.
+
 USAGE_END
     exit 0;
 }
+
+usage;
+
+exit 0;
