98f473670ad25a8fe277d5fa2b91f757c3a64e53
Alexander Kuleshov <kuleshovmail@gmail.com>
2019-03-06 13:16:28 +0600

    general: remove everything from elpa
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacs.d/elpa/dash-20170613.151/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (22905 43429 882173 422000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
