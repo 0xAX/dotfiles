1a157231d8a38a7d741f1b21a44ccd3cb5a3e4ad
Alexander Kuleshov <kuleshovmail@gmail.com>
2019-12-22 19:34:05 +0600

    emacs: remove rust mode
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/dev/rust.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'load-path "~/.emacs.d/rust")
-(autoload 'rust-mode "rust-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
