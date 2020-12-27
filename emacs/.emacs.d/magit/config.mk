7870dc51f49246c796542b4da3c6deb2deed2c73
Alexander Kuleshov <kuleshovmail@gmail.com>
2020-12-27 17:27:50 +0600

    push config.mk for magit

---
+++ emacs/.emacs.d/magit/config.mk
@@ -0,0 +1,9 @@
+LOAD_PATH = -L ~/.emacs.d/magit/lisp
+LOAD_PATH += -L ~/.emacs.d/dash.el
+# build transient and with-editor:
+#
+#  $ make lisp
+#
+LOAD_PATH += -L ~/.emacs.d/magit-deps/transient/lisp
+LOAD_PATH += -L ~/.emacs.d/magit-deps/with-editor
+LOAD_PATH += -L ~/.emacs.d/magit-deps/libegit2
