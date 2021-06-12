905a1949a8c0e5bbbeff0492e0cf55baaf67b327
Alexander Kuleshov <kuleshovmail@gmail.com>
2021-06-12 18:42:26 +0600

    emacs: remove magit from .emacs.d
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacs.d/magit/config.mk
+++
@@ -1,9 +0,0 @@
-LOAD_PATH = -L ~/.emacs.d/magit/lisp
-LOAD_PATH += -L ~/.emacs.d/dash.el
-# build transient and with-editor:
-#
-#  $ make lisp
-#
-LOAD_PATH += -L ~/.emacs.d/magit-deps/transient/lisp
-LOAD_PATH += -L ~/.emacs.d/magit-deps/with-editor
-LOAD_PATH += -L ~/.emacs.d/magit-deps/libegit2
