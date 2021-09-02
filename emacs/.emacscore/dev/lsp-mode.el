fdc62c3c689c5187fb27ae726f5477c6e745cb89
Alexander Kuleshov <kuleshovmail@gmail.com>
2021-09-02 13:21:53 +0600

    lsp-mode: make it working for Elixir
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/dev/lsp-mode.el
+++ emacs/.emacscore/dev/lsp-mode.el
@@ -4,9 +4,9 @@
 
 (when (file-exists-p "/etc/fedora-release")
   (when (string= (shell-command-to-string "command -v clangd") "")
-    (sudo-shell-command "dnf install clang-tools-extra"))
+    (sudo-shell-command "dnf install clang-tools-extra -y"))
   (when (string= (shell-command-to-string "command -v bear") "")
-    (sudo-shell-command "dnf install bear")))
+    (sudo-shell-command "dnf install bear -y")))
 
 ;; Setup keybindings for lsp-mode
 (setq lsp-keymap-prefix "C-c C-l")
@@ -30,10 +30,19 @@
           (lambda (frame _w)
             (set-face-attribute 'default frame :font "Firacode" :height 150)))
 
+;; lsp ui configuration
 (setq lsp-ui-doc-header t)
 (setq lsp-ui-doc-show-with-cursor t)
 (setq lsp-ui-doc-include-signature t)
 (setq lsp-ui-doc-use-childframe t)
+
+  (setq lsp-file-watch-ignored
+        '(".idea" ".ensime_cache" ".eunit" "node_modules"
+          ".git" ".hg" ".fslckout" "_FOSSIL_"
+          ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "vendor" "doc"
+          "build" "_build" "deps" "postgres-data" "dia" "/home/alex/work/tposs/src")
+        )
+
 
 ;; TODO lsp-ui-doc-focus-frame
 ;; TODO lsp-ui-doc-unfocus-frame
