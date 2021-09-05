5aff8cdcafd2ec0fd59fd7fa8b91d7efb73ac817
Alexander Kuleshov <kuleshovmail@gmail.com>
2021-09-05 13:41:04 +0600

    emacs: unbind some default keybindings in magit
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/vcs/magit.el
+++ emacs/.emacscore/vcs/magit.el
@@ -3,4 +3,18 @@
 ;; For more information see:
 ;;  * https://magit.vc/manual/transient/Enabling-and-Disabling-Suffixes.html
 ;;  * https://emacsair.me/2019/02/14/transient-0.1/
-(setq transient-default-level 5)
+
+(require 'magit)
+
+;; Enable more suffixes
+(setq transient-default-level 7)
+
+;; unbind magit-section keybindings as they are used to navigate
+;; in my i3 setup
+(with-eval-after-load "magit"
+  (define-key magit-section-mode-map (kbd "M-1") nil)
+  (define-key magit-section-mode-map (kbd "M-2") nil)
+  (define-key magit-section-mode-map (kbd "M-3") nil)
+  (define-key magit-section-mode-map (kbd "M-4") nil)
+  (define-key magit-section-mode-map [C-tab] nil)
+  (define-key magit-section-mode-map [M-tab] nil))
