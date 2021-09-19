ab39bc4b2200448f0c7db96ff772a7f600a4d093
Alexander Kuleshov <kuleshovmail@gmail.com>
2021-09-19 19:46:37 +0600

    emacs: fix typo in ui.el
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/ui.el
+++ emacs/.emacscore/ui.el
@@ -13,10 +13,7 @@
                                         (dired-directory dired-directory
                                                          (revert-buffer-function " %b" ("%b – Dir:  " default-directory)))))))
 
-;; allow recursive command execution
-(setq enable-recursive-minibuffers t)
-
-;; set path to solarized them
+;; set path to solarized theme
 (setq solarized-theme-path
       (concat user-emacs-directory "/straight/build/solarized-emacs"))
 
