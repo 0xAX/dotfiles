c0ac7c1dc042dc231922ab1a8a7b85461e2025ee
Alexander Kuleshov <kuleshovmail@gmail.com>
2023-03-12 15:18:23 +0600

    emacs: fix update-system-dotfiles function name
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/keybindings.el
+++ emacs/.emacscore/keybindings.el
@@ -101,7 +101,7 @@
 
 ;; dotfiles repo
 (global-set-key (kbd "C-d C-u") 'update-dotfiles-repo)
-(global-set-key (kbd "C-d C-s") 'update-dotfiles-system)
+(global-set-key (kbd "C-d C-s") 'update-system-dotfiles)
 
 ;; magit keybindings
 (global-set-key (kbd "C-x C-l") 'magit-log-buffer-file)
