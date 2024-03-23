9185d02927428034a8c54cb9ab416c1601f692cb
Alexander Kuleshov <kuleshovmail@gmail.com>
2024-03-23 15:17:29 +0500

    emacs: remove commented code from org-mode configuration
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/org/orgmode.el
+++ emacs/.emacscore/org/orgmode.el
@@ -64,28 +64,3 @@
 
 (plist-put org-format-latex-options :foreground nil)
 (plist-put org-format-latex-options :background nil)
-;; (setq org-preview-latex-process-alist
-;;        '((dvipng :programs
-;;          ("lualatex" "dvipng")
-;;          :description "dvi > png" :message "you need to install the programs: latex and dvipng." :image-input-type "dvi" :image-output-type "png" :image-size-adjust
-;;          (1.0 . 1.0)
-;;          :latex-compiler
-;;          ("lualatex -output-format dvi -interaction nonstopmode -output-directory %o %f")
-;;          :image-converter
-;;          ("dvipng -fg %F -bg %B -D %D -T tight -o %O %f"))
-;;        (dvisvgm :programs
-;;           ("latex" "dvisvgm")
-;;           :description "dvi > svg" :message "you need to install the programs: latex and dvisvgm." :use-xcolor t :image-input-type "xdv" :image-output-type "svg" :image-size-adjust
-;;           (1.7 . 1.5)
-;;           :latex-compiler
-;;           ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
-;;           :image-converter
-;;           ("dvisvgm %f -n -b min -c %S -o %O"))
-;;        (imagemagick :programs
-;;               ("latex" "convert")
-;;               :description "pdf > png" :message "you need to install the programs: latex and imagemagick." :use-xcolor t :image-input-type "pdf" :image-output-type "png" :image-size-adjust
-;;               (1.0 . 1.0)
-;;               :latex-compiler
-;;               ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
-;;               :image-converter
-;;               ("convert -density %D -trim -antialias %f -quality 100 %O"))))
