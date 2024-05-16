0f8fed0a3d2d13595950b07f0ec71f01ab8a083e
Alexander Kuleshov <kuleshovmail@gmail.com>
2024-05-17 00:46:57 +0500

    emacs: add local keybindings for rust-mode
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/dev/rust.el
+++ emacs/.emacscore/dev/rust.el
@@ -8,9 +8,18 @@
 ;; format the code on save
 (setq rustic-format-on-save t)
 
+;; set compilation command
+(setq rustic-compile-command "cargo build")
+
 ;; prettify some symbols
-(add-hook 'rust-mode-hook
-          (lambda () (prettify-symbols-mode)))
+(add-hook 'rustic-mode-hook
+          (lambda ()
+            (prettify-symbols-mode)))
+
+;; rust-mode keybindings
+(define-key rustic-mode-map (kbd "C-c b") 'rustic-cargo-bench)
+(define-key rustic-mode-map (kbd "C-c c") 'rustic-compile)
+(define-key rustic-mode-map (kbd "C-c t") 'rustic-cargo-test-run)
 
 ;; enable LSP for rust if we have rust-analyzer
 (when (executable-find "rust-analyzer")
