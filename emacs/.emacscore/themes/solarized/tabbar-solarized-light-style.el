875f3b5dbda1491cc8af303944e2e494b834b52e
Alexander Kuleshov <kuleshovmail@gmail.com>
2024-06-04 00:06:55 +0500

    emacs: fix tabbar
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/themes/solarized/tabbar-solarized-light-style.el
+++ emacs/.emacscore/themes/solarized/tabbar-solarized-light-style.el
@@ -3,47 +3,60 @@
 ;; Tabbar mode
 (require 'tabbar)
 
-;; All tabs in one group
+;; Group similar files together in tabbar
 (defun my-tabbar-buffer-groups ()
-  (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
-              ((eq major-mode 'dired-mode) "emacs")
-              ((string-equal ".sh" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "system")
-              ((string-equal ".h" (substring (buffer-name) (- (length (buffer-name)) 2) (length (buffer-name)))) "system")
-              ((string-equal ".c" (substring (buffer-name) (- (length (buffer-name)) 2) (length (buffer-name)))) "system")
-              ((string-equal ".cc" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "system")
-              ((string-equal ".hh" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "system")
-              ((string-equal ".cpp" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "system")
-              ((string-equal ".hpp" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "system")
-              ((string-equal ".S" (substring (buffer-name) (- (length (buffer-name)) 2) (length (buffer-name)))) "system")
-              ((string-equal ".s" (substring (buffer-name) (- (length (buffer-name)) 2) (length (buffer-name)))) "system")
-              ((string-equal ".asm" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "system")
-              ((string-equal ".ld" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "system")
-              ((string-equal "Makefile" (substring (buffer-name) (- (length (buffer-name)) 8) (length (buffer-name)))) "system")
-              ((string-equal ".mk" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "system")
-              ((string-equal "Kbuild" (substring (buffer-name) (- (length (buffer-name)) 6) (length (buffer-name)))) "system")
-              ((string-equal ".inc" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "system")
-              ((string-equal "KConfig" (substring (buffer-name) (- (length (buffer-name)) 7) (length (buffer-name)))) "system")
-              ((string-equal "Kconfig" (substring (buffer-name) (- (length (buffer-name)) 7) (length (buffer-name)))) "system")
-              ((string-equal "*t" (substring (buffer-name) (- (length (buffer-name)) 2) (length (buffer-name)))) "terminal")
-              ((string-equal ".el" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "lisp")
-              ((string-equal ".emacs" (substring (buffer-name) (- (length (buffer-name)) 6) (length (buffer-name)))) "lisp")
-              ((string-equal ".lisp" (substring (buffer-name) (- (length (buffer-name)) 5) (length (buffer-name)))) "lisp")
-              ((string-equal ".erl" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "erlang")
-              ((string-equal ".ex" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "erlang")
-              ((string-equal ".exs" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "erlang")
-              ((string-equal ".hrl" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "erlang")
-              ((string-equal ".json" (substring (buffer-name) (- (length (buffer-name)) 5) (length (buffer-name)))) "web")
-              ((string-equal ".js" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "web")
-              ((string-equal ".html" (substring (buffer-name) (- (length (buffer-name)) 5) (length (buffer-name)))) "web")
-              ((string-equal ".css" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "web")
-              ((string-equal ".tpl.html" (substring (buffer-name) (- (length (buffer-name)) 9) (length (buffer-name)))) "web")
-              ((string-equal ".rb" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "web")
-              ((string-equal ".php" (substring (buffer-name) (- (length (buffer-name)) 5) (length (buffer-name)))) "web")
-              ((string-equal ".go" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "golang")
-              ((string-equal ".md" (substring (buffer-name) (- (length (buffer-name)) 3) (length (buffer-name)))) "doc")
-              ((string-equal ".rst" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "doc")
-              ((string-equal ".org" (substring (buffer-name) (- (length (buffer-name)) 4) (length (buffer-name)))) "doc")
-              (t "user"))))
+  (list (cond
+         ;; Emacs buffers
+         ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
+         ((eq major-mode 'dired-mode) "emacs")
+
+         ;; system programming files
+         ((string-equal "sh"       (file-name-extension (buffer-name))) "system")
+         ((string-equal "h"        (file-name-extension (buffer-name))) "system")
+         ((string-equal "c"        (file-name-extension (buffer-name))) "system")
+         ((string-equal "cc"       (file-name-extension (buffer-name))) "system")
+         ((string-equal "hh"       (file-name-extension (buffer-name))) "system")
+         ((string-equal "cpp"      (file-name-extension (buffer-name))) "system")
+         ((string-equal "hpp"      (file-name-extension (buffer-name))) "system")
+         ((string-equal "S"        (file-name-extension (buffer-name))) "system")
+         ((string-equal "s"        (file-name-extension (buffer-name))) "system")
+         ((string-equal "asm"      (file-name-extension (buffer-name))) "system")
+         ((string-equal "ld"       (file-name-extension (buffer-name))) "system")
+         ((string-equal "mk"       (file-name-extension (buffer-name))) "system")
+         ((string-equal "inc"      (file-name-extension (buffer-name))) "system")
+         ((string-equal "KConfig"  (buffer-name)) "system")
+         ((string-equal "Kconfig"  (buffer-name)) "system")
+         ((string-equal "Kbuild"   (buffer-name)) "system")
+         ((string-equal "Makefile" (buffer-name)) "system")
+
+         ;; lisp programming
+         ((string-equal "el"       (file-name-extension (buffer-name))) "lisp")
+         ((string-equal "lisp"     (file-name-extension (buffer-name))) "lisp")
+         ((string-equal "emacs"    (buffer-name)) "lisp")
+
+         ;; erlang programming
+         ((string-equal "erl"      (file-name-extension (buffer-name))) "erlang")
+         ((string-equal "ex"       (file-name-extension (buffer-name))) "erlang")
+         ((string-equal "exs"      (file-name-extension (buffer-name))) "erlang")
+         ((string-equal "hrl"      (file-name-extension (buffer-name))) "erlang")
+
+         ;; web proggramming
+         ((string-equal "json"     (file-name-extension (buffer-name))) "web")
+         ((string-equal "js"       (file-name-extension (buffer-name))) "web")
+         ((string-equal "html"     (file-name-extension (buffer-name))) "web")
+         ((string-equal "css"      (file-name-extension (buffer-name))) "web")
+         ((string-equal "tpl.html" (file-name-extension (buffer-name))) "web")
+
+         ;; golang programming
+         ((string-equal "go"       (file-name-extension (buffer-name))) "golang")
+
+         ;; documentation
+         ((string-equal "md"       (file-name-extension (buffer-name))) "documentation")
+         ((string-equal "rst"      (file-name-extension (buffer-name))) "documentation")
+         ((string-equal "org"      (file-name-extension (buffer-name))) "documentation")
+
+         ;; everything else
+         (t "user"))))
 
 ;; set up custom tabbar group
 (setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
