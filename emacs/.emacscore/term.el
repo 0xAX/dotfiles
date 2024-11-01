47b535b68d8248594c1d0918a662a4d6fcbf3ad7
Alexander Kuleshov <kuleshovmail@gmail.com>
2024-11-01 17:31:02 +0500

    emacs: add vterm configuration
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/term.el
+++ emacs/.emacscore/term.el
@@ -1,37 +1,41 @@
 ;;; term.el --- terminal emulator configuration in GNU Emacs  -*- lexical-binding: t -*-
 
+(require 'vterm)
+
 ;; bash is a standard shell
 (setq explicit-shell-file-name "/bin/bash")
 (setq shell-file-name "/bin/bash")
 (setenv "SHELL" shell-file-name)
 
-(defadvice ansi-term (after advise-ansi-term-coding-system activate)
-  (set-process-coding-system 'utf-8-unix 'utf-8-unix))
-
-;; close the terminal buffer automatically on exit
-(defadvice term-sentinel (around my-advice-term-sentinel (proc msg) activate)
-  (if (memq (process-status proc) '(signal exit))
-	  (let ((buffer (process-buffer proc)))
-		ad-do-it
-		(kill-buffer buffer))
-	ad-do-it))
-
-;; we need to scroll
-(setq term-buffer-maximum-size 0) 
-
 ;; Improves terminal emulator (vterm/eat) throughput
 (setq read-process-output-max (* 2 1024 1024)
       process-adaptive-read-buffering nil
-      ;; Improve scrolling and other UI responsiveness
       fast-but-imprecise-scrolling t
       redisplay-skip-fontification-on-input t
       inhibit-compacting-font-caches t
       idle-update-delay 1.0)
 
-;; Open new terminal with name
 (defun open-terminal (terminal-name)
   "Open terminal with custom buffer name"
-  (interactive
-   (list
-	(read-string "Buffer name: ")))
-    (ansi-term (getenv "SHELL") (concat "term: " terminal-name)))
+    (interactive "sTerminal name: ")
+    (vterm (concat "term-" terminal-name)))
+
+;; vterm custom keybindings
+(define-key vterm-mode-map (kbd "C-c C-v") #'vterm-yank)
+
+;; unset some common keybindings for vterm
+(add-hook 'vterm-mode-hook
+          (lambda ()
+            ;; unbind keybinding we use to switch between emacs tabs
+            (local-unset-key (kbd "<C-left>"))
+            (local-unset-key (kbd "<C-right>"))
+            ;; unbind keybinding to switch between i3 workspaces
+            (local-unset-key (kbd "M-1"))
+            (local-unset-key (kbd "M-2"))
+            (local-unset-key (kbd "M-3"))
+            (local-unset-key (kbd "M-4"))
+            (local-unset-key (kbd "M-5"))
+            (local-unset-key (kbd "M-6"))
+            (local-unset-key (kbd "M-7"))
+            (local-unset-key (kbd "M-8"))
+            (local-unset-key (kbd "M-9"))))
