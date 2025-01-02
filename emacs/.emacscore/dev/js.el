0f9bfa7e813f5bfaa75cefe7047cc27a07e22ac5
Alexander Kuleshov <kuleshovmail@gmail.com>
2025-01-02 15:13:23 +0500

    emacs: add some comments why we use *.sys.mjs configuration
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/dev/js.el
+++ emacs/.emacscore/dev/js.el
@@ -1,3 +1,4 @@
 ;;; js.el --- SLIME configuration -*- lexical-binding: t -*-
 
+;; Highlight javascript files from the Mozilla Firefox source code
 (add-to-list 'auto-mode-alist '("\\.sys\\.mjs\\'" . javascript-mode))
