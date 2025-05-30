d24a9b41679ea8c3cdf4bdf046b3c4f5ee696eaa
Alexander Kuleshov <kuleshovmail@gmail.com>
2025-05-30 23:29:51 +0500

    lsp: enable language server for Elixir
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/dev/elixir.el
+++ emacs/.emacscore/dev/elixir.el
@@ -5,10 +5,10 @@
 (setq lsp-elixir-script-path
       "/home/alex/dev/elixir-ls/release/language_server.sh")
 
-;; (when (file-exists-p lsp-elixir-script-path)
-;;   (add-hook 'elixir-mode-hook 'lsp)
-;;   (setq lsp-elixir-local-server-command
-;;         lsp-elixir-script-path)
-;;   (setq lsp-elixir-dialyzer-enabled t)
-;;   (setq lsp-elixir-fetch-deps t)
-;;   (setq lsp-elixir-mix-env "test"))
+(when (file-exists-p lsp-elixir-script-path)
+  (add-hook 'elixir-mode-hook 'lsp)
+  (setq lsp-elixir-local-server-command
+        lsp-elixir-script-path)
+  (setq lsp-elixir-dialyzer-enabled t)
+  (setq lsp-elixir-fetch-deps t)
+  (setq lsp-elixir-mix-env "test"))
