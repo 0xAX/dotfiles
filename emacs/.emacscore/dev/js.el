e0bf9f5292172caabd45c7390c231ffbe0abc792
Alexander Kuleshov <kuleshovmail@gmail.com>
2025-07-01 19:28:44 +0500

    emacs: use 2 spaces to indend javascript
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/dev/js.el
+++ emacs/.emacscore/dev/js.el
@@ -2,3 +2,6 @@
 
 ;; Highlight javascript files from the Mozilla Firefox source code
 (add-to-list 'auto-mode-alist '("\\.sys\\.mjs\\'" . javascript-mode))
+
+;; Set indentation for javascript
+(setq js-indent-level 2)
