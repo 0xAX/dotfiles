5b3c13bca406942279ca549969a29447d94d72fc
Alexander Kuleshov <kuleshovmail@gmail.com>
2025-07-20 15:31:49 +0500

    emacs: add lsp for ts
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/dev/js.el
+++ emacs/.emacscore/dev/js.el
@@ -5,3 +5,28 @@
 
 ;; Set indentation for javascript
 (setq js-indent-level 2)
+
+(defun npm-and-typescript-ls-installed ()
+  "Check that `npm` is installed and that `typescript` and `typescript-language-server`
+are globally installed."
+  (interactive)
+  (let ((npm-exists (executable-find "npm")))
+    (if (not npm-exists)
+        (progn
+          (warn "npm is not installed or not in PATH.")
+          nil)
+      (let ((npm-list-output (shell-command-to-string "npm list -g --depth=0 --parseable")))
+        (and
+         (seq-filter (lambda (package) (string= (file-name-base package) "typescript"))
+                     (string-split test-ts-var "\n"))
+         (seq-filter (lambda (package) (string= (file-name-base package) "typescript-language-server"))
+                     (string-split test-ts-var "\n")))))))
+
+;; Enable LSP for javascript/typescript
+(when (npm-and-typescript-ls-installed)
+  (use-package lsp-mode
+    :hook ((typescript-mode . lsp)
+           (tsx-mode . lsp))
+    :commands lsp
+    :init
+    (setq lsp-keymap-prefix "C-c l")))
