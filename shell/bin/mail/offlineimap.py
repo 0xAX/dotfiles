d60cdce8a21e1202c156dae065fa6ea768128e1a
Alexander Kuleshov <kuleshovmail@gmail.com>
2025-08-10 02:32:22 +0500

    mail: update scripts
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- shell/bin/mail/offlineimap.py
+++ shell/bin/mail/offlineimap.py
@@ -1,5 +1,5 @@
 import subprocess
 
 def get_pass():
-    passwd = subprocess.getoutput('op item get qvmlw4o5342mdtofsdpxaorvdi | grep password | sed -e "s/  password:    //"')
+    passwd = subprocess.getoutput('op item get qvmlw4o5342mdtofsdpxaorvdi --reveal | grep password | sed -e "s/  password:    //"')
     return passwd
