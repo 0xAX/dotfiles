cfd3f955358f72266359a4c19e031e47dd4f610f
Alexander Kuleshov <kuleshovmail@gmail.com>
2025-08-10 17:33:10 +0500

    emacs: add mu4e keybindings
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/keybindings.el
+++ emacs/.emacscore/keybindings.el
@@ -30,7 +30,7 @@
 (global-set-key (kbd "C-x b")   'browse-url)
 (global-set-key (kbd "C-x p")   'ping)
 (global-set-key (kbd "C-x l")   'list-colors-display)
-(global-set-key (kbd "C-x m")   'woman)
+(global-set-key (kbd "C-x m")   'mu4e)
 (global-set-key (kbd "C-x e")   'eshell)
 
 ;; Text manipulation and cursor manipulation
