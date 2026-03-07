544326db634ff95ce2fed6409129ad1f6c591695
Alexander Kuleshov <kuleshovmail@gmail.com>
2026-03-07 16:12:01 +0500

    emacs: execute sudo more securely
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/system.el
+++ emacs/.emacscore/system.el
@@ -1,4 +1,15 @@
 ;;; system.el --- auxiliary functions for system management -*- lexical-binding: t -*-
 
 (defun sudo-shell-command (command)
-  (shell-command (concat "echo " (read-passwd "Password: ") " | sudo -S " command)))
+  "Execute COMMAND with sudo privileges securely.
+Uses a temporary file to pass the password to avoid exposure in process lists."
+  (let ((temp-file (make-temp-file "emacs-sudo-"))
+        (password (read-passwd "Password: ")))
+    (unwind-protect
+        (progn
+          (with-temp-file temp-file
+            (insert password))
+          (set-file-modes temp-file #o600)
+          (shell-command (format "sudo -S %s < %s" command temp-file)))
+      (when (file-exists-p temp-file)
+        (delete-file temp-file)))))
