441229430797382cce23aaf8840ce264d681b76d
Alexander Kuleshov <kuleshovmail@gmail.com>
2026-03-07 16:11:05 +0500

    emacs: prevent new tabs opened during switching between directories in dired
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- emacs/.emacscore/dired.el
+++ emacs/.emacscore/dired.el
@@ -11,6 +11,13 @@
 ;; open file in a dired
 (put 'dired-find-alternate-file 'disabled nil)
 
+;; Reuse the same buffer when navigating directories
+;; This prevents creating tons of tabs/buffers
+(add-hook 'dired-mode-hook
+          (lambda ()
+            (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+            (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))))
+
 ;; Use nerd fonts in dired if possible
 (when
     (or
